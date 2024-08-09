7290645c923c4d5f7ba3418ee80d3baf0288d86b
giorgimuktiashvili <giorgimuqtiashvili@gmail.com>
2024-08-09 15:48:23 +0400

    Rem: deleted unnecessary files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Dropdown from "./Components/Dropdown/Dropdown";
 import InfoCard from "./Components/InfoCard/InfoCard";
-import InfoCardRounded from "./Components/InfoCardRounded/InfoCardRounded";
 import styles from "./page.module.css";
 
 export default function Home() {
