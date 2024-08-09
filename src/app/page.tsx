94a228101aca8e430f2edb24446fa4cd68dbc0a5
Luka Tchelidze <lukachelidze3008@gmail.com>
2024-08-09 14:07:26 +0400

    fix:removed navmenu from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import NavMenu from "./Components/NavMenu/NavMenu";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main>
-      <NavMenu/>
     </main>
   );
 }
