cb024d6a5e183c0ece7c5089966d39c31a005521
Luka Tchelidze <lukachelidze3008@gmail.com>
2024-08-10 13:24:50 +0400

    feat:Created Artist Table

---
+++ src/app/interface/artistTable.interface.ts
@@ -0,0 +1,10 @@
+export interface ArtistTableInterFace {
+    id: number;
+    key: string;
+    image: string;
+    name: string;
+    albumName: string;
+    musics: string;
+    likes: string;
+    view: string;
+}
