82eeaeaf37583d88698b20dcef95bac02cab623d
Luka Tchelidze <lukachelidze3008@gmail.com>
2024-08-13 13:33:02 +0400

    refactor:changed table

--- src/app/Components/UserTable/UserTable.tsx
+++ src/app/Components/UserTable/UserTable.tsx
@@ -3,60 +3,28 @@ import React from 'react';
 import { Table, Space } from 'antd';
 import styles from './UserTable.module.scss';
 import { UserTableInterFace } from '@/app/interface/UserTable.interface';
-import ArtistUserInfoCard from '../ArtistUserInfoCard/ArtistUserInfoCard';
 import MultiTaskButton from '../MultiTaskButton/MultiTaskButton';
 
 const dataSource: UserTableInterFace[] = [
     {
         id: 1,
         key: '1',
-        image: '/images/user1.png',
-        name: 'User Name',
-        status: 'Activity',
         email: 'UserGmail@gmail.com',
-        number: '555 555 555',
-        activity: '1h Ago',
     },
     {
         id: 2,
         key: '2',
-        image: '/images/user2.png',
-        name: 'User Name',
-        status: 'Activity',
         email: 'UserGmail@gmail.com',
-        number: '555 555 555',
-        activity: '1h Ago',
     },
     {
         id: 2,
         key: '3',
-        image: '/images/user3.png',
-        name: 'User Name',
-        status: 'Activity',
         email: 'UserGmail@gmail.com',
-        number: '555 555 555',
-        activity: '1h Ago',
     },
 
 ];
 
 const columns = [
-    {
-        title: 'Name/Image',
-        dataIndex: 'name',
-        key: 'name',
-        render: (text: string, record: UserTableInterFace) => (
-            <ArtistUserInfoCard image={record.image} userName={record.name} />
-        ),
-    },
-    {
-        title: 'Status',
-        dataIndex: 'status',
-        key: 'status',
-        render : (text:string, record:UserTableInterFace)=>(
-            <span className={styles.text}>{record.status}</span>
-        )
-    },
     {
         title: 'Gmail',
         dataIndex: 'email',
@@ -65,22 +33,7 @@ const columns = [
             <span className={styles.text}>{record.email}</span>
         )
     },
-    {
-        title: 'Number',
-        dataIndex: 'number',
-        key: 'number',
-        render : (text:string, record:UserTableInterFace)=>(
-            <span className={styles.text}>{record.number}</span>
-        )
-    },
-    {
-        title: 'Activity',
-        dataIndex: 'activity',
-        key: 'activity',
-        render : (text:string, record:UserTableInterFace)=>(
-            <span className={styles.text}>{record.activity}</span>
-        )
-    },
+    
     {
         title: 'Actions',
         key: 'actions',
--- src/app/interface/UserTable.interface.ts
+++ src/app/interface/UserTable.interface.ts
@@ -1,10 +1,6 @@
 export interface UserTableInterFace {
     id: number;
     key: string;
-    image: string;
-    name: string;
-    status: string;
     email: string;
-    number: string;
-    activity: string;
+
 }
