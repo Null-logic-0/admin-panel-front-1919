07cb9409f621b9d3c12449d246ae335bacb9f6dc
Luka Tchelidze <lukachelidze3008@gmail.com>
2024-08-16 10:37:50 +0400

    refactor:added Modals for user Page

--- src/app/Components/UserTable/UserTable.tsx
+++ src/app/Components/UserTable/UserTable.tsx
@@ -1,59 +1,83 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { Table, Space } from 'antd';
 import styles from './UserTable.module.scss';
 import { UserTableInterFace } from '@/app/interface/UserTable.interface';
 import MultiTaskButton from '../MultiTaskButton/MultiTaskButton';
+import Dropdown from '../Dropdown/Dropdown';
+import { dropDownOptions } from '@/app/interface/dropdown.interface';
 
-const dataSource: UserTableInterFace[] = [
-    {
-        id: 1,
-        key: '1',
-        email: 'UserGmail@gmail.com',
-    },
-    {
-        id: 2,
-        key: '2',
-        email: 'UserGmail@gmail.com',
-    },
-    {
-        id: 2,
-        key: '3',
-        email: 'UserGmail@gmail.com',
-    },
 
-];
 
-const columns = [
-    {
-        title: 'Gmail',
-        dataIndex: 'email',
-        key: 'email',
-        render : (text:string, record:UserTableInterFace)=>(
-            <span className={styles.text}>{record.email}</span>
-        )
-    },
-    
-    {
-        title: 'Actions',
-        key: 'actions',
-        render: (text: any, record: UserTableInterFace) => (
-            <Space size='middle'>
-                <MultiTaskButton icon={'/Icons/pen.svg'} />
-                <MultiTaskButton icon={'/Icons/trash.svg'} />
-            </Space>
-        ),
-    },
-];
+type userTableProps = {
+    dataSource: UserTableInterFace[];
+    remove?: () => void;
+    dropdownOptions: dropDownOptions[];
+};
 
-const UserTable = () => {
+const UserTable = ({ dataSource, remove,dropdownOptions}: userTableProps) => {
+    const [dropdownVisibility, setDropdownVisibility] = useState<{ [key: string]: boolean }>({});
+
+    const toggleDropdown = (id: string) => {
+        setDropdownVisibility(prevState => ({
+            ...prevState,
+            [id]: !prevState[id],
+        }));
+    };
+
+    const closeDropdown = (id: string) => {
+        setDropdownVisibility(prevState => ({
+            ...prevState,
+            [id]: false,
+        }));
+    };
+
+    const columns = [
+        {
+            title: 'Gmail',
+            dataIndex: 'email',
+            key: 'email',
+            render: (text: string, record: UserTableInterFace) => (
+                <span className={styles.text}>{record.email}</span>
+            ),
+        },
+        {
+            title: 'Actions',
+            key: 'actions',
+            render: (text: any, record: UserTableInterFace) => (
+                <Space size='middle'>
+                    <div className={styles.dropdownContainer}>
+                        <div className={styles.button}>
+                            <MultiTaskButton 
+                                icon={'/Icons/pen.svg'} 
+                                onclick={() => toggleDropdown(record.id.toString())} 
+                            />
+                        </div>
+
+                        {dropdownVisibility[record.id.toString()] && (
+                            <div className={styles.dropdown}>
+                                <Dropdown options={dropdownOptions}  onOptionSelect={() => closeDropdown(record.id.toString())}/>
+                            </div>
+                        )}
+                    </div>
+
+                    <MultiTaskButton 
+                        icon={'/Icons/trash.svg'} 
+                        onclick={remove} 
+                    />
+                </Space>
+            ),
+        },
+    ];
+
     return (
         <Table
             dataSource={dataSource}
             columns={columns}
             pagination={false}
-            rowKey="key"
-            className={styles.tableContainer} />
+            rowKey="id" 
+            className={styles.tableContainer}
+        />
     );
 };
 
